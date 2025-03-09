f7d8a0117192a2961a1b92d6b6b213edf950fb2d
TotevM <mtotev53@gmail.com>
2025-03-09 19:16:49 +0200

    Project separation

---
+++ SPP.Data/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SPP.Data.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FullName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; }
+    }
+}
---
+++ SPP.Data/Models/Payment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPP.Data.Models
+{
+    public class Payment
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public Guid PlanId { get; set; }
+        public decimal Amount { get; set; }
+        public string Currency { get; set; }
+        public string PaymentStatus { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public string StripeSessionId { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
+        public virtual Plan Plan { get; set; }
+    }
+}
