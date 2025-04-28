67255a85c39b11330beabba32a66725882049c53
TotevM <mtotev53@gmail.com>
2025-04-28 23:03:25 +0300

    Built Products Index Page

--- SPP.Web/Controllers/ProductController.cs
+++ SPP.Web/Controllers/ProductController.cs
@@ -1,12 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SPP.ViewModels;
 
 namespace SPP.Web.Controllers
 {
-	public class ProductController : Controller
-	{
-		public IActionResult Index()
-		{
-			return View();
-		}
-	}
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public ProductController(ApplicationDbContext _context)
+        {
+            context = _context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await context.Products.Select(x => new ProductViewModel
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Price = x.Price,
+                ImageUrl = x.ImageUrl
+            }).ToListAsync();
+
+            return View(products);
+        }
+    }
 }
