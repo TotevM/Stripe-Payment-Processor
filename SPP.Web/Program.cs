f2b0039161e939899683b6951e6ea70d97f118f7
Metodi Totev <mtotev53@gmail.com>
2025-05-07 11:11:21 +0300

    Auto-apply migrations

--- SPP.Web/Program.cs
+++ SPP.Web/Program.cs
@@ -1,58 +1,11 @@
-//using Microsoft.AspNetCore.Identity;
-//using Microsoft.EntityFrameworkCore;
-//using SPP.Data.Models;
-//using StripePaymentProcessor.Data;
-
-//var builder = WebApplication.CreateBuilder(args);
-
-//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
-//                      ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-
-//builder.Services.AddDbContext<ApplicationDbContext>(options =>
-//    options.UseSqlServer(connectionString));
-
-////builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-//builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
-//    .AddEntityFrameworkStores<ApplicationDbContext>()
-//    .AddDefaultTokenProviders().AddDefaultUI();
-
-//builder.Services.AddRazorPages();
-//builder.Services.AddControllersWithViews();
-
-//var app = builder.Build();
-
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseMigrationsEndPoint();
-//}
-//else
-//{
-//    app.UseExceptionHandler("/Home/Error");
-//    app.UseHsts();
-//}
-
-//app.UseHttpsRedirection();
-//app.UseStaticFiles();
-
-//app.UseRouting();
-
-//app.UseAuthentication();
-
-//app.UseAuthorization();
-
-//app.MapControllerRoute(
-//    name: "default",
-//    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-//app.MapRazorPages();
-
-//app.Run();
 
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using SPP.Data.Models;
 using StripePaymentProcessor.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -93,6 +46,22 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+app.ApplyMigrations();
 app.MapRazorPages();
 
 app.Run();
+
+public static class ApplicationBuilderExtensions
+{
+	public static IApplicationBuilder ApplyMigrations(this IApplicationBuilder app)
+	{
+		using IServiceScope serviceScope = app.ApplicationServices.CreateScope();
+
+		ApplicationDbContext dbContext = serviceScope
+			.ServiceProvider
+			.GetRequiredService<ApplicationDbContext>()!;
+		dbContext.Database.Migrate();
+
+		return app;
+	}
+} //TODO: move into the extension project
